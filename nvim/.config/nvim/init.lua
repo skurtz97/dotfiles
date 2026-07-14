f289879541cd568fd4f7d53b18885ff55706c1f5
Sean Kurtz <seankurtz75@gmail.com>
2026-07-14 10:08:04 -0500

    Lua autocompletion and nvim setup.

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -2,20 +2,29 @@
 -- Neovim Configuration (init.lua)
 -- ============================================================================
 
--- Bootstrap lazy.nvim (Modern plugin manager)
+-- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-
--- Safely resolve Neovim's filesystem API without linter confusion
-local fs = vim.uv or vim.loop
-
-if not fs.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable",
-    lazypath,
-  })
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({ "git", "clone", "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
 end
 vim.opt.rtp:prepend(lazypath)
+
+-- ============================================================================
+-- Core Plugin Setup
+-- ============================================================================
+require("lazy").setup({
+  -- The Fix: Add lazydev.nvim to your plugins
+  { "folke/lazydev.nvim", ft = "lua", opts = {} },
+  
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+      require("catppuccin").setup({ flavour = "mocha" })
+      vim.cmd.colorscheme("catppuccin")
+    end,
+  },
+})
+
