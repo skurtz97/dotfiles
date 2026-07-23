cfeee72e909e505500eeec50e62b54a023bb59b1
Sean Kurtz <seankurtz75@gmail.com>
2026-07-22 23:49:28 -0500

    Changed CursorLine to darker color.

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -8,5 +8,6 @@ vim.opt.colorcolumn = { "71", "80", "100", "120" }
 vim.api.nvim_create_autocmd("ColorScheme", {
   callback = function()
     vim.api.nvim_set_hl(0, "ColorColumn", { bg = "#1f2126" })
+    vim.api.nvim_set_hl(0, "CursorLine", { bg = "#1c1e24" })
   end,
 })
